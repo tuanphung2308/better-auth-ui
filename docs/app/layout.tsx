b5f63cd77b9a34a93eeacb24e10f9e66ee51d2f2
daveycodez <david@swiftapk.com>
2025-03-05 12:51:24 -0800

    Add screenshots to introduction

--- docs/app/layout.tsx
+++ docs/app/layout.tsx
@@ -19,7 +19,7 @@ export default function Layout({ children }: { children: ReactNode }) {
                         },
                     }}
                     theme={{
-
+                        attribute: "class"
                     }}
                 >
                     {children}
