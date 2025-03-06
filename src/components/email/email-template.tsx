d7e3d82eecbaef4156cb2aa3b121bd6154cce038
daveycodez <david@swiftapk.com>
2025-03-05 21:01:22 -0800

    EmailTemplate heading & classNames props

--- src/components/email/email-template.tsx
+++ src/components/email/email-template.tsx
@@ -13,12 +13,28 @@ import {
     Tailwind,
     Text
 } from "@react-email/components"
+import type { ReactNode } from "react"
+
+import { cn } from "../../lib/utils"
+
+export interface EmailTemplateClassNames {
+    body?: string
+    button?: string
+    container?: string
+    content?: string
+    footer?: string
+    heading?: string
+    hr?: string
+    link?: string
+}
 
 export interface EmailTemplateProps {
+    classNames?: EmailTemplateClassNames
     action: string
     /** @default process.env.BASE_URL || process.env.NEXT_PUBLIC_BASE_URL */
     baseUrl?: string
-    content: string
+    content: ReactNode
+    heading: ReactNode
     /** @default `${baseUrl}/apple-touch-icon.png` */
     imageUrl?: string
     preview?: string
@@ -30,9 +46,11 @@ export interface EmailTemplateProps {
 }
 
 export const EmailTemplate = ({
+    classNames,
     action,
     baseUrl,
     content,
+    heading,
     imageUrl,
     preview,
     siteName,
@@ -42,6 +60,7 @@ export const EmailTemplate = ({
     baseUrl = baseUrl || process.env.BASE_URL || process.env.NEXT_PUBLIC_BASE_URL
     imageUrl = imageUrl || `${baseUrl}/apple-touch-icon.png`
     siteName = siteName || process.env.SITE_NAME || process.env.NEXT_PUBLIC_SITE_NAME
+    preview = preview || (typeof heading == "string" ? heading : undefined)
 
     return (
         <Html>
@@ -109,7 +128,7 @@ export const EmailTemplate = ({
             )}
 
             <Tailwind>
-                <Body className="my-auto mx-auto font-sans px-2">
+                <Body className={cn("my-auto mx-auto font-sans px-2", classNames?.body)}>
                     <Container className="border border-solid border-color rounded my-[40px] mx-auto p-[20px] max-w-[465px]">
                         <Section className="mt-[32px]">
                             <Img
@@ -121,34 +140,38 @@ export const EmailTemplate = ({
                             />
                         </Section>
 
-                        <Heading className="text-[24px] font-normal text-center p-0 my-[30px] mx-0">
-                            <strong>
-                                {preview}
-                            </strong>
+                        <Heading className={cn("text-[24px] font-bold text-center p-0 my-[30px] mx-0", classNames?.heading)}>
+                            {heading}
                         </Heading>
 
-                        <Text className="text-[14px] leading-[24px]">
+                        <Text className={cn("text-[14px] leading-[24px]", classNames?.content)}>
                             {content}
                         </Text>
 
                         <Section className="text-center mt-[32px] mb-[32px]">
                             <Button
-                                className="action-button rounded text-[12px] font-semibold no-underline text-center px-5 py-3"
+                                className={cn("action-button rounded text-[12px] font-semibold no-underline text-center px-5 py-3", classNames?.button)}
                                 href={url}
                             >
                                 {action}
                             </Button>
                         </Section>
 
-                        <Hr className="border border-solid border-color my-[26px] mx-0 w-full" />
+                        <Hr className={cn("border border-solid border-color my-[26px] mx-0 w-full", classNames?.hr)} />
 
-                        <Text className="text-[#666666] text-[12px] leading-[24px]">
-                            {siteName}
-                            {" "}
+                        <Text className={cn("text-[#666666] text-[12px] leading-[24px]", classNames?.footer)}>
+                            {siteName && (
+                                <>
+                                    {siteName}
+                                    {" "}
+                                </>
+                            )}
 
-                            <Link className="no-underline" href={baseUrl}>
-                                {baseUrl?.replace("https://", "").replace("http://", "")}
-                            </Link>
+                            {baseUrl && (
+                                <Link className={cn("no-underline", classNames?.link)} href={baseUrl}>
+                                    {baseUrl?.replace("https://", "").replace("http://", "")}
+                                </Link>
+                            )}
                         </Text>
                     </Container>
                 </Body>
