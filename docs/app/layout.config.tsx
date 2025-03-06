f33e9264d8c1feffbfba7f24306810f541116719
daveycodez <david@swiftapk.com>
2025-03-05 23:59:20 -0800

    Update layout.config.tsx

--- docs/app/layout.config.tsx
+++ docs/app/layout.config.tsx
@@ -12,7 +12,15 @@ export const baseOptions: BaseLayoutProps = {
     githubUrl: "https://github.com/daveyplate/better-auth-ui",
     nav: {
         // can be JSX too!
-        title: "better-auth-ui",
+        title: (
+            <>
+                <svg className="w-5 h-5" fill="none" height="45" viewBox="0 0 60 45" width="60" xmlns="http://www.w3.org/2000/svg">
+                    <path className="fill-black dark:fill-white" clipRule="evenodd" d="M0 0H15V15H30V30H15V45H0V30V15V0ZM45 30V15H30V0H45H60V15V30V45H45H30V30H45Z" fill-rule="evenodd" />
+                </svg>
+                BETTER-AUTH UI.
+            </>
+
+        ),
     },
     links: [
         {
