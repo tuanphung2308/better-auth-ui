71f2efce9ef5f88895fb6501a0701f278a8ccbd1
daveycodez <david@swiftapk.com>
2025-03-06 01:56:42 -0800

    Different logo

--- docs/app/layout.config.tsx
+++ docs/app/layout.config.tsx
@@ -14,7 +14,16 @@ export const baseOptions: BaseLayoutProps = {
         // can be JSX too!
         title: (
             <>
-                BETTER-AUTH-UI
+                <svg className="w-5 h-5" fill="none" height="45" viewBox="0 0 60 45" width="60" xmlns="http://www.w3.org/2000/svg">
+                    <path
+                        className="fill-black dark:fill-white"
+                        clipRule="evenodd"
+                        d="M0 0H15V45H0V0ZM45 0H60V45H45V0ZM20 0H40V15H20V0ZM20 30H40V45H20V30Z"
+                        fillRule="evenodd"
+                    />
+                </svg>
+
+                BETTER-AUTH. UI
             </>
 
         ),
