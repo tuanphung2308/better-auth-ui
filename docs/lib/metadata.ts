8619ba4012937a70b1310bf796108d9bbb6bd120
daveycodez <david@swiftapk.com>
2025-03-07 14:34:10 -0800

    Update metadata.ts

--- docs/lib/metadata.ts
+++ docs/lib/metadata.ts
@@ -6,17 +6,17 @@ export function createMetadata(override: Metadata): Metadata {
         openGraph: {
             title: override.title ?? undefined,
             description: override.description ?? undefined,
-            url: "https://better-auth.com",
-            images: "https://better-auth.com/og.png",
-            siteName: "Better Auth",
+            url: "https://better-auth-ui.com",
+            images: "https://better-auth-ui.com/better-auth-ui-promo-dark.png",
+            siteName: "Better Auth UI",
             ...override.openGraph
         },
         twitter: {
             card: "summary_large_image",
-            creator: "@beakcru",
+            creator: "@daveycodez",
             title: override.title ?? undefined,
             description: override.description ?? undefined,
-            images: "https://better-auth.com/og.png",
+            images: "https://better-auth-ui.com/better-auth-ui-promo-dark.png",
             ...override.twitter
         }
     }
