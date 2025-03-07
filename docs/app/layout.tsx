ac169f716cd6545b03f5fa4f632cf21b29175f82
daveycodez <david@swiftapk.com>
2025-03-06 22:39:44 -0800

    Fix page title for docs

--- docs/app/layout.tsx
+++ docs/app/layout.tsx
@@ -3,11 +3,21 @@ import { Geist } from "next/font/google"
 import type { ReactNode } from "react"
 
 import "./global.css"
+import { baseUrl, createMetadata } from "@/lib/metadata"
 
 const geistSans = Geist({
     subsets: ["latin"]
 })
 
+export const metadata = createMetadata({
+    title: {
+        template: "%s | Better Auth UI",
+        default: "Better Auth UI"
+    },
+    description:
+        "UI components for the most comprehensive authentication library for TypeScript.",
+    metadataBase: baseUrl
+})
 export default function Layout({ children }: { children: ReactNode }) {
     return (
         <html lang="en" suppressHydrationWarning>
