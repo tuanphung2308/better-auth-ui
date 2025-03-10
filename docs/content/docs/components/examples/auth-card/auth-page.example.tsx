6aedf9e11e78dd8bef43bdcc180e459a806478a4
daveycodez <david@swiftapk.com>
2025-03-10 03:36:30 -0700

    Fix docs for AuthCard

--- docs/content/docs/components/examples/auth-card/auth-page.example.tsx
+++ docs/content/docs/components/examples/auth-card/auth-page.example.tsx
@@ -2,7 +2,7 @@ import { AuthCard } from "@daveyplate/better-auth-ui"
 
 export default function AuthPage({ params }: { params: { pathname: string } }) {
     return (
-        <div className="flex justify-center my-auto">
+        <div className="flex flex-col grow size-full items-center justify-center gap-3">
             <AuthCard pathname={params.pathname} />
         </div>
     )
