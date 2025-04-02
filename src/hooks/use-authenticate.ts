98b937e6dade9f405cda282f7d238c45e1cd9370
daveycodez <david@swiftapk.com>
2025-04-02 00:46:58 -0700

    Cleaner useAuthenticate

--- src/hooks/use-authenticate.ts
+++ src/hooks/use-authenticate.ts
@@ -1,37 +1,17 @@
 import { useContext, useEffect } from "react"
 import { AuthUIContext } from "../lib/auth-ui-provider"
 import type { AuthView } from "../server"
-import type { AuthClient } from "../types/auth-client"
 
-export function useAuthenticate<TAuthClient extends AuthClient = AuthClient>(
-    authView: AuthView = "signIn",
-    enabled = true
-) {
+export function useAuthenticate(authView: AuthView = "signIn") {
     const { hooks, basePath, viewPaths, replace } = useContext(AuthUIContext)
     const { useSession } = hooks
-    const { data, isPending, ...rest } = useSession()
+    const { data, isPending } = useSession()
 
     useEffect(() => {
-        if (enabled && !isPending && !data) {
+        if (!isPending && !data) {
             replace(
                 `${basePath}/${viewPaths[authView]}?redirectTo=${window.location.href.replace(window.location.origin, "")}`
             )
         }
-    }, [enabled, isPending, data, basePath, viewPaths, replace, authView])
-
-    type SessionData = TAuthClient["$Infer"]["Session"]
-    type User = TAuthClient["$Infer"]["Session"]["user"]
-    type Session = TAuthClient["$Infer"]["Session"]["session"]
-
-    const sessionData = data as SessionData | undefined
-    const user = sessionData?.user as User | undefined
-    const session = sessionData?.session as Session | undefined
-
-    return {
-        data: sessionData,
-        isPending,
-        ...rest,
-        user,
-        session
-    }
+    }, [isPending, data, basePath, viewPaths, replace, authView])
 }
