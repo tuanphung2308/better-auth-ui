4c72513b4fc927001288cfbdff2a3c5630d4b572
daveycodez <david@swiftapk.com>
2025-04-04 13:04:31 -0700

    useAuthenticate accepts enabled option

--- src/hooks/use-authenticate.ts
+++ src/hooks/use-authenticate.ts
@@ -2,16 +2,25 @@ import { useContext, useEffect } from "react"
 import { AuthUIContext } from "../lib/auth-ui-provider"
 import type { AuthView } from "../server"
 
-export function useAuthenticate(authView: AuthView = "signIn") {
-    const { hooks, basePath, viewPaths, replace } = useContext(AuthUIContext)
-    const { useSession } = hooks
+export function useAuthenticate({
+    authView = "signIn",
+    enabled = true
+}: { authView?: AuthView; enabled?: boolean }) {
+    const {
+        hooks: { useSession },
+        basePath,
+        viewPaths,
+        replace
+    } = useContext(AuthUIContext)
     const { data, isPending } = useSession()
 
     useEffect(() => {
+        if (!enabled) return
+
         if (!isPending && !data) {
             replace(
                 `${basePath}/${viewPaths[authView]}?redirectTo=${window.location.href.replace(window.location.origin, "")}`
             )
         }
-    }, [isPending, data, basePath, viewPaths, replace, authView])
+    }, [isPending, data, basePath, viewPaths, replace, authView, enabled])
 }
