bc14be8ea0a4f6831afdf57b1743810c3d9cbe2a
daveycodez <daveycodez@gmail.com>
2025-04-11 02:14:24 -0700

    Update update-avatar-card.tsx

--- src/components/settings/update-avatar-card.tsx
+++ src/components/settings/update-avatar-card.tsx
@@ -176,7 +176,7 @@ export function UpdateAvatarCard({
 
             <CardFooter
                 className={cn(
-                    "!py-4 md:!py-5 rounded-b-xl border-t bg-muted dark:bg-transparent",
+                    "!py-5 rounded-b-xl border-t bg-muted dark:bg-transparent",
                     classNames?.footer
                 )}
             >
