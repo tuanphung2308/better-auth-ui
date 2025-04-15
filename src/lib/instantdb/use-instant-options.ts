8e9e70ca94549f41573ba74803efd04bf5a0b750
daveycodez <daveycodez@gmail.com>
2025-04-15 15:12:03 -0700

    Add updateUser mutator to useInstantOptions

--- src/lib/instantdb/use-instant-options.ts
+++ src/lib/instantdb/use-instant-options.ts
@@ -1,6 +1,7 @@
 import type { InstantReactWebDatabase } from "@instantdb/react"
 import type { Session, User } from "../../types/auth-client"
 import type { AuthHooks } from "../../types/auth-hooks"
+import type { AuthMutators } from "../../types/auth-mutators"
 import { useListAccounts } from "./use-list-accounts"
 import { useListSessions } from "./use-list-sessions"
 import { useSession as useInstantSession } from "./use-session"
@@ -18,6 +19,7 @@ export interface UseInstantOptionsProps {
     modelNames?: Partial<ModelNames>
     usePlural?: boolean
     sessionData?: { user: User; session: Session }
+    user?: { id: string } | null
     isPending: boolean
 }
 
@@ -26,8 +28,11 @@ export function useInstantOptions({
     usePlural,
     modelNames,
     sessionData,
-    isPending
+    isPending,
+    user
 }: UseInstantOptionsProps) {
+    const userId = user?.id || sessionData?.user.id
+
     return {
         hooks: {
             useSession: () =>
@@ -54,6 +59,20 @@ export function useInstantOptions({
                     sessionData,
                     isPending
                 })
-        } as AuthHooks
+        } as AuthHooks,
+        mutators: {
+            updateUser: async (data) => {
+                if (!userId) {
+                    throw new Error("Unauthenticated")
+                }
+
+                db.transact([
+                    db.tx.users[userId].update({
+                        ...data,
+                        updatedAt: Date.now()
+                    })
+                ])
+            }
+        } as AuthMutators
     }
 }
