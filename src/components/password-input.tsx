0e10527670cddb536b618357fec63a74e0d686cf
daveycodez <daveycodez@gmail.com>
2025-04-22 04:43:54 -0700

    Fix Password Eye background

--- src/components/password-input.tsx
+++ src/components/password-input.tsx
@@ -31,7 +31,7 @@ export function PasswordInput({
             {enableToggle && (
                 <>
                     <Button
-                        className="absolute top-0 right-0 hover:bg-transparent"
+                        className="!bg-transparent absolute top-0 right-0"
                         disabled={disabled}
                         size="icon"
                         type="button"
