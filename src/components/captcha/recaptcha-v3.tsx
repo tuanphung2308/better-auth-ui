44fc955f18b6390d6ac3d11e66a97a0fe7ec7cff
daveycodez <daveycodez@gmail.com>
2025-05-10 01:31:28 -0700

    Update recaptcha-v3.tsx

--- src/components/captcha/recaptcha-v3.tsx
+++ src/components/captcha/recaptcha-v3.tsx
@@ -14,6 +14,7 @@ export function RecaptchaV3() {
     useEffect(() => {
         if (initialized) return
         if (captcha?.provider !== "google-recaptcha-v3") return
+        if (typeof grecaptcha === "undefined") return
 
         const checkInitialized = () => {
             const iframe = document.querySelector("iframe[title='reCAPTCHA']") as HTMLIFrameElement
